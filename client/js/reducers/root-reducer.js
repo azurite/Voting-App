7e63e112a012cab6219354ae4f7d3dc87bea5a08
Marko Nikic <darkblue.azurite@gmail.com>
2016-12-01 17:12:34 +0100

    partially added polleditor

--- client/js/reducers/root-reducer.js
+++ client/js/reducers/root-reducer.js
@@ -1,12 +1,14 @@
 const pollReducer = require("./polls-reducer");
 const loginReducer = require("./login-reducer");
 const registerReducer = require("./register-reducer");
+const profileReducer = require("./profile-reducer");
 
 const rootReducer = function(state, action) {
   return {
     polls: pollReducer(state.polls, action),
     login: loginReducer(state.login, action),
-    register: registerReducer(state.register, action)
+    register: registerReducer(state.register, action),
+    profile: profileReducer(state.profile, action)
   };
 };
 
