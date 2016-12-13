07241e162313f67fcae1a617929b0585bf14644c
Marko Nikic <darkblue.azurite@gmail.com>
2016-12-13 16:15:40 +0100

    removed all files that are in gitignore

--- build/client/style.b8b04ffdfe57a5acb77d.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,3],[function(n,c){}]);
-//# sourceMappingURL=style.b8b04ffdfe57a5acb77d.js.map
--- client/js/dev/sampleAccount.js
+++
@@ -1,9 +0,0 @@
-const ownPolls = require("./samplePolls");
-
-const sampleAccount = {
-  email: "example@email.com",
-  id: 123456,
-  ownPolls: ownPolls
-};
-
-module.exports = sampleAccount;
